a5f78b4d2e88dbd552f80fb283c2f895492eb6c5
NohSungBeen <castlekong1019@gmail.com>
2025-01-05 20:07:47 +0900

    #14 test : ArticleController 테스트 기능 추가
    view 이름까지 테스트 하도록 추가했습니다.

--- src/test/java/com/study/board/controller/ArticleControllerTest.java
+++ src/test/java/com/study/board/controller/ArticleControllerTest.java
@@ -29,7 +29,8 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(view().name("articles/index"));
     }
 
     @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상 호출")
@@ -39,7 +40,9 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
     @DisplayName("[view] [GET] 게시글 검색 전용 페이지 - 정상 호출")
@@ -48,7 +51,8 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
     }
 
     @DisplayName("[view] [GET] 게시글 해시태그 검색 페이지 - 정상 호출")
@@ -57,7 +61,8 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
     }
 
 }
