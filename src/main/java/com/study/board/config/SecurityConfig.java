90f8b5bd99012b1544e714f94acf8aaec3f4c795
NohSungBeen <castlekong1019@gmail.com>
2025-01-15 19:51:18 +0900

    #14 build : 시큐리티 의존성 추가 및 설정 추가

---
+++ src/main/java/com/study/board/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.study.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 모든 요청을 허용
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+        return http.build();
+    }
+}
